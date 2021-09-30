49a24c841180d0e077879e912200291b987491f7
Abhinandan Singla <deepusingla0448@gmail.com>
2021-09-30 14:43:01 +0530

    intial

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:vendo/screens/Home/HomePage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+import 'package:vendo/screens/Home/NewMP.dart';
 import 'providers/mainProvider.dart';
 
 void main() async {
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: HomePage(),
+        home: HomePage1(),
       ),
     );
   }
